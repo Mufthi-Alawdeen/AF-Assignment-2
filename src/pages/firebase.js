3eccf615be7a223fc669e8f7fb6de7e777b821fc
Mufthi Alawdeen <168330217+Mufthi-Alawdeen@users.noreply.github.com>
2025-05-04 19:51:22 +0530

    Update firebase.js

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Add this
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // Add this
 
 const firebaseConfig = {
   apiKey: "AIzaSyAkAlKef9jDECe0nJqNiOy-lGWc780Rt3I",
   authDomain: "countires-817ff.firebaseapp.com",
   projectId: "countires-817ff",
-  storageBucket: "countires-817ff.firebasestorage.app",
+  storageBucket: "countires-817ff.appspot.com",
   messagingSenderId: "710818121949",
   appId: "1:710818121949:web:fab8b8b6821e866dc1b334",
   measurementId: "G-1Q6R99BCQT",
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); // Add this export
-export default app;
+export const db = getFirestore(app);
+export const auth = getAuth(app); // Export auth
